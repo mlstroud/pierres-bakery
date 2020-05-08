2708b3bc7f056a25c56729f93f73f26ceb9eefa4
Matt Stroud <lixingke06@gmail.com>
2020-05-08 09:07:44 -0700

    refactor to create Order class to hold static order information

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -2,39 +2,78 @@ using System.Collections.Generic;
 
 namespace Bakery.Models
 {
+  public class Order
+  {
+    public static List<Bread> purchasedBread;
+    public static List<Pastry> purchasedPastry;
+
+    public Order()
+    {
+      purchasedItems = new List<Bread>();
+      purchasedPastry = new List<Pastry>();
+    }
+
+    public static double CalculatePurchaseCost()
+    {
+
+    }
+
+    public static double CalculateBreadTotal()
+    {
+      int totalItems = 0;
+      double currentTotal = 0;
+      int itemCounter = 1;
+
+      foreach (Bread item in purchasedItems)
+      {
+        if (itemCounter <= 2)
+        {
+          currentTotal += item.Cost;
+          itemCounter++;
+        }
+        else
+        {
+          itemCounter = 1;
+        }
+      }
+
+      return currentTotal;
+    }
+
+    public static double CalculatePastryTotal()
+    {
+      int totalItems = purchasedPastry.Count;
+      double currentTotal = 0;
+      int itemCounter = 1;
+
+      if (totalItems > 3)
+      {
+
+      }
+      else
+      {
+
+      }
+
+
+      return currentTotal;
+    }
+  }
+
   public class Bread
   {
     public string Name { get; set; }
     public double Cost { get; set; }
-    public static List<Bread> purchasedItems;
 
     public Bread(string name, double cost)
     {
       Name = name;
       Cost = cost;
-      purchasedItems = new List<Bread>();
     }
 
     public void Purchase()
     {
       purchasedItems.Add(this);
-    }
-
-    public static double CalculatePurchaseCost()
-    {
-      int totalItems = 0;
-      double currentTotal = 0;
-
-      foreach (Bread item in purchasedItems)
-      {
-        totalItems++;
-        if (totalItems % 2 == 1)
-        {
-          currentTotal += item.Cost;
-        }
-      }
-
-      return currentTotal;
     }
   }
 
