8ae970ee5b822b9d27107b0e248a979bc85e2146
Matt Stroud <lixingke06@gmail.com>
2020-05-08 08:20:39 -0700

    add Cost property and constructor to Bread and Pastry classses

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -2,11 +2,21 @@ namespace Bakery.Models
 {
   public class Bread
   {
+    public int Cost { get; set; }
 
+    public Bread()
+    {
+
+    }
   }
 
   public class Pastry
   {
+    public int Cost { get; set; }
 
+    public Pastry()
+    {
+
+    }
   }
 }
