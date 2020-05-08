71d8a93fbe2d9cfb7e65daa9249f7cbd6a3d6769
Matt Stroud <lixingke06@gmail.com>
2020-05-08 08:52:08 -0700

    add method to Purchase current item, and method to Calculate total purchase cost

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -1,23 +1,49 @@
+using System.Collections.Generic;
+
 namespace Bakery.Models
 {
   public class Bread
   {
     public string Name { get; set; }
-    public float Cost { get; set; }
+    public double Cost { get; set; }
+    public static List<Bread> purchasedItems;
 
-    public Bread(string name, float cost)
+    public Bread(string name, double cost)
     {
       Name = name;
       Cost = cost;
+      purchasedItems = new List<Bread>();
+    }
+
+    public void Purchase()
+    {
+      purchasedItems.Add(this);
+    }
+
+    public static double CalculatePurchaseCost()
+    {
+      int totalItems = 0;
+      double currentTotal = 0;
+
+      foreach (Bread item in purchasedItems)
+      {
+        totalItems++;
+        if (totalItems % 2 == 1)
+        {
+          currentTotal += item.Cost;
+        }
+      }
+
+      return currentTotal;
     }
   }
 
   public class Pastry
   {
     public string Name { get; set; }
-    public float Cost { get; set; }
+    public double Cost { get; set; }
 
-    public Pastry(string name, float cost)
+    public Pastry(string name, double cost)
     {
       Name = name;
       Cost = cost;
